504ccc84aacabb69e27265fe1b18c04c201d2220
Bruno Martucci <brunomartucci77@gmail.com>
2024-01-04 12:40:28 -0300

    First commit

--- src/Main.java
+++ src/Main.java
@@ -1,16 +1,57 @@
+import java.sql.SQLOutput;
+import java.util.Scanner;
 
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        Scanner scanner = new Scanner(System.in);
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+        System.out.println("CALCULADORA SIMPLES EM JAVA");
+        System.out.println("---------------------------");
+
+        while (true) {
+            System.out.println("Digite o primeiro número");
+            double num1 = scanner.nextDouble();
+            System.out.println("Digite o operador +, -, *, /");
+            char operador = scanner.next().charAt(0);
+            System.out.println("Digite o segundo numero");
+            double num2 = scanner.nextDouble();
+
+            double resultado = 0;
+
+            switch (operador) {
+                case '+':
+                    resultado = num1 + num2;
+                    break;
+                case '-':
+                    resultado = num1 - num2;
+                    break;
+                case '*':
+                    resultado = num1 * num2;
+                    break;
+                case '/':
+                    if (num2 != 0) {
+                        resultado = num1 / num2;
+                    } else {
+                        System.out.println("Erro: Divisão por zero");
+                        continue;
+                    }
+                    break;
+                default:
+                    System.out.println("Operador invalido. Tente novamente");
+                    continue;
+            }
+            System.out.println("Resultado: " + resultado);
+
+            System.out.println("Deseja realizar outra ação ? (S/N) ");
+            char resposta = scanner.next().charAt(0);
+
+            if (resposta != 'S' && resposta !='s'){
+                System.out.println("Encerrando a calculadora");
+                break;
+            }
         }
+        scanner.close();
     }
 }
